70e1dde833350e3051e12729cc81a24033ebf227
mpramos <mpramos78915400@gmail.com>
2019-08-13 13:20:29 -0400

    Repositorio inicial de los scripts

---
+++ Maria/Scripts/Maria/Scripts/AtaqueVida.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtaqueVida : MonoBehaviour
+{
+   public int dañoEnemigo=18;
+   GameObject Mplayer;
+   Vida playerVida;
+
+    // Start is called before the first frame update
+   private void Awake(){
+       Mplayer=GameObject.FindGameObjectWithTag("Player");
+       playerVida=Mplayer.GetComponent<Vida>();
+        
+   }
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other){
+        if(other.CompareTag("Player"))
+        {
+            playerVida.TomaDaño(dañoEnemigo);
+        }
+    }
+}
---
+++ Maria/Scripts/Maria/Scripts/Vida.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vida : MonoBehaviour
+{
+    public int SaludInicial=100;
+    public int SaludActual;
+    public Slider barraSlider;
+    
+    private void Awake(){
+        SaludActual=SaludInicial;
+    }
+    public void TomaDaño(int valorDaño){
+        SaludActual -=valorDaño;
+        barraSlider.value=SaludActual;
+        if(SaludActual<=0)
+            Destroy(gameObject);
+    }
+
+
+}
